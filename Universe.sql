7a4315d15e8ccd0b8b5b11653b134c738b5f76e4
Stjepan Glavaš <sglavas@m.ffzg.hr>
2025-08-11 15:26:54 +0200

    Update Universe.sql

--- Universe.sql
+++ Universe.sql
@@ -12,7 +12,7 @@ SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
-SET xmloption = content;
+SET xmloption = content;1
 SET client_min_messages = warning;
 SET row_security = off;
 
